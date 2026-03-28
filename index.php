f93ed9c9bb9d1a30bfff73c00c90309ed68788d9
aidenwolf887 <madebija098@gmail.com>
2026-03-28 20:53:41 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -7,5 +7,11 @@
 <body>
     <h1>Hello world! Davindra.</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum consectetur blanditiis voluptatum ipsa veniam officia explicabo obcaecati laboriosam temporibus, sunt vero ratione cupiditate odit id quo magnam perspiciatis minus cumque.</p>
+    <ul>
+        <li>
+        Satu
+    </li>
+    <li>Dua</li>
+</ul>
 </body>
 </html>
